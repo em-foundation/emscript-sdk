720b5bd851238639cea6bf5c55834a9f739ad7da
build-sdk.yml workflow <hello@openem.org>
2025-03-02 10:54:28 +0000

    build-sdk run: 26.2.0.202503021053

--- workspace/em.core/em.examples.combo/Ex01_HelloBlinkerP.em.ts
+++ workspace/em.core/em.examples.combo/Ex01_HelloBlinkerP.em.ts
@@ -3,15 +3,28 @@ export const $U = em.$declare('MODULE')
 
 import * as BoardC from '@$distro/BoardC.em'
 import * as Common from '@em.mcu/Common.em'
+import * as TimeTypes from '@em.utils/TimeTypes.em'
 
 export const AppLed = $delegate(BoardC.AppLed)
 
 export function em$run() {
     for (let i = 0; ; i++) {
         if (i % 10 == 0) {
-            printf`hello world %d\n`(i / 2)
+            printTime(Common.Uptimer.$$.read())
+            printf` hello world %d\n`(i / 2)
         }
         Common.BusyWait.$$.wait(500_000)
         AppLed.$$.toggle()
     }
+}
+
+function printTime(rawTime: TimeTypes.RawTime) {
+    const timeParts = TimeTypes.RawTimeToTimeParts(rawTime)
+    printf`%dT%02d:%02d:%02d.%03d`(
+        timeParts.days,
+        timeParts.hours,
+        timeParts.minutes,
+        timeParts.seconds,
+        timeParts.milliseconds
+    )
 }
