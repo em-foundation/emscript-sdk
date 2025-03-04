8269cd6942ebfa936a32d03351eba12560e5ea5d
build-sdk.yml workflow <hello@openem.org>
2025-03-04 20:24:29 +0000

    build-sdk run: 26.2.0.202503042024

--- workspace/adi.max326xx/adi.distro.max326xx/BuildC.em.ts
+++ workspace/adi.max326xx/adi.distro.max326xx/BuildC.em.ts
@@ -1,5 +1,6 @@
 import em from '@$$emscript'
 import { userInfo } from 'os'
+import { execSync } from 'child_process'
 export const $U = em.$declare('COMPOSITE')
 
 import * as ArmStartupC from '@em.arch.arm/StartupC.em'
@@ -105,11 +106,22 @@ export function em$generate() {
     `)
     out.close()
     //
+    const getDriveLetter = (driveLabel: string) => {
+      try {
+        const stdout = execSync(`wmic logicaldisk where volumename="${driveLabel}" get caption /value`).toString()
+        const lines = stdout.trim().split('\r\n')
+        if (lines.length && lines[0].startsWith('Caption=')) {
+          return lines[0].split('=')[1].trim()
+        }
+        return null
+      } catch (error) {
+        console.error('Error:', error)
+        return null
+      }
+    }
     const load_folder =
       process.platform === 'win32'
-        // TODO: This only works if the DAPLINK USB disk mounts as D: on the windows system
-        // figure out how to get the mounted volume labeled DAPLINK
-        ? '/d'
+        ? `/${getDriveLetter('DAPLINK')?.toLowerCase().replace(':', '') || 'd'}`
         : process.platform === 'linux'
           ? `/media/${userInfo().username}/DAPLINK/`
           : '/Volumes/daplink'
