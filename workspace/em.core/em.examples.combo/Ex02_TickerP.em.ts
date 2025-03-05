ac34652028167791c4bc013c8cd058c118f61911
build-sdk.yml workflow <hello@openem.org>
2025-03-05 13:13:58 +0000

    build-sdk run: 26.2.0.202503051313

--- workspace/em.core/em.examples.combo/Ex02_TickerP.em.ts
+++ workspace/em.core/em.examples.combo/Ex02_TickerP.em.ts
@@ -8,42 +8,40 @@ import * as TickerMgr from '@em.utils/TickerMgr.em'
 import * as TimeTypes from '@em.utils/TimeTypes.em'
 
 // app settings
-const SYS_LED_PERIOD_MS = 1500
-const APP_LED_PERIOD_MS = 2000
-const PRINT_PERIOD_MS = 5000
-const MIN_PRESS_TIME_MS = 10
-const MAX_PRESS_TIME_MS = 2000
 const MAX_DIVIDED_BY = 8
+const MAX_PRESS_TIME_MS = 2000
+const MIN_PRESS_TIME_MS = 10
+const TICKER_APP_PERIOD_MS = 2000
+const TICKER_PRINT_PERIOD_MS = 5000
+const TICKER_SYS_PERIOD_MS = 1500
 
 // app resources
-const AppLed = $delegate(BoardC.AppLed)
-const AppBut = $delegate(BoardC.AppBut)
-const SysLed = $delegate(BoardC.SysLed)
-
-const app_ticker = $config<TickerMgr.Obj>()
-const sys_ticker = $config<TickerMgr.Obj>()
-const print_ticker = $config<TickerMgr.Obj>()
+const button = $delegate(BoardC.AppBut)
+const led_app = $delegate(BoardC.AppLed)
+const led_sys = $delegate(BoardC.SysLed)
+const ticker_app = $config<TickerMgr.Obj>()
+const ticker_print = $config<TickerMgr.Obj>()
+const ticker_sys = $config<TickerMgr.Obj>()
 
 export namespace em$meta {
     export function em$construct() {
-        app_ticker.$$ = TickerMgr.em$meta.create()
-        sys_ticker.$$ = TickerMgr.em$meta.create()
-        print_ticker.$$ = TickerMgr.em$meta.create()
+        ticker_app.$$ = TickerMgr.em$meta.create()
+        ticker_print.$$ = TickerMgr.em$meta.create()
+        ticker_sys.$$ = TickerMgr.em$meta.create()
     }
 }
 
 // initial state vector
+let count_app = 0
+let count_print = 0
+let count_sys = 0
 let divided_by = 1
-let sys_count = 0
-let app_count = 0
-let last_app_count = 0
-let last_sys_count = 0
-let print_count = 0
-let total_errors = 0
+let expected_count_app: u32
+let expected_count_sys: u32
+let last_count_app = 0
+let last_count_sys = 0
 let prints_after_rate_change = 0
-
-let expected_app_count: u32
-let expected_sys_count: u32
+let total_errors = 0
 
 export function em$run() {
     printf`\nEx01_TickerP program startup\n\n`()
@@ -54,24 +52,18 @@ export function em$run() {
     FiberMgr.run()
 }
 
-function appTickCb() {
-    app_count += 1
-    $['%%c']
-    AppLed.$$.wink(10)
-}
-
 function countError(count: u32, expectedCount: u32): bool_t {
     return count < expectedCount || count > expectedCount + 1
 }
 
 function onButtonPressed() {
-    if (AppBut.$$.isPressed()) {
+    if (button.$$.isPressed()) {
         // a long press (press time > max_press_time_ms)
         printf`Long button press: Stopping app/sys tickers\n`()
         divided_by = 0
         stopLedTickers()
-        last_app_count = 0
-        last_sys_count = 0
+        last_count_app = 0
+        last_count_sys = 0
     } else {
         // a short press (min_press_time_ms < press time < max_press_time_ms)
         divided_by =
@@ -90,48 +82,16 @@ function printStatus() {
     )
     printf`Current rate %dx\n`(divided_by)
     printf`... should print every ~%ds\n`(
-        PRINT_PERIOD_MS / TimeTypes.MILLISECONDS_PER_SECOND
+        TICKER_PRINT_PERIOD_MS / TimeTypes.MILLISECONDS_PER_SECOND
     )
     printf`... app ticks should be %d..%d\n`(
-        expected_app_count,
-        expected_app_count + 1
+        expected_count_app,
+        expected_count_app + 1
     )
     printf`... sys ticks should be %d..%d\n`(
-        expected_sys_count,
-        expected_sys_count + 1
+        expected_count_sys,
+        expected_count_sys + 1
     )
-}
-
-function printTickCb() {
-    print_count += 1
-    prints_after_rate_change++
-    let this_app_count = app_count - last_app_count
-    let this_sys_count = sys_count - last_sys_count
-    let this_app_error = countError(this_app_count, expected_app_count)
-    let this_sys_error = countError(this_sys_count, expected_sys_count)
-    if (prints_after_rate_change > 2 && (this_app_error || this_sys_error)) {
-        this_app_error && total_errors++
-        this_sys_error && total_errors++
-    }
-    printTime(Common.Uptimer.$$.read())
-    printf` Print tick {rate: %dx, ticks: {app: %d%s, sys: %d%s}, errors: %d}\n`(
-        divided_by,
-        this_app_count,
-        this_app_error ? t$`*` : t$``,
-        this_sys_count,
-        this_sys_error ? t$`*` : t$``,
-        total_errors
-    )
-    if (divided_by > 0 && last_sys_count > 0 && last_sys_count == sys_count) {
-        printf`No sys ticks detected since last print\n`()
-        halt()
-    }
-    if (divided_by > 0 && last_app_count > 0 && last_app_count == app_count) {
-        printf`No app ticks detected since last print\n`()
-        halt()
-    }
-    last_app_count = app_count
-    last_sys_count = sys_count
 }
 
 function printTime(rawTime: TimeTypes.RawTime) {
@@ -146,7 +106,7 @@ function printTime(rawTime: TimeTypes.RawTime) {
 }
 
 function startButton() {
-    AppBut.$$.onPressed(
+    button.$$.onPressed(
         $cb(onButtonPressed),
         MIN_PRESS_TIME_MS,
         MAX_PRESS_TIME_MS
@@ -154,34 +114,70 @@ function startButton() {
 }
 
 function startLedTickers() {
-    app_ticker.$$.$$.start(
-        TimeTypes.Secs24p8_initMsecs(APP_LED_PERIOD_MS) / divided_by,
-        $cb(appTickCb)
+    ticker_app.$$.$$.start(
+        TimeTypes.Secs24p8_initMsecs(TICKER_APP_PERIOD_MS) / divided_by,
+        $cb(tickCbApp)
     )
-    sys_ticker.$$.$$.start(
-        TimeTypes.Secs24p8_initMsecs(SYS_LED_PERIOD_MS) / divided_by,
-        $cb(sysTickCb)
+    ticker_sys.$$.$$.start(
+        TimeTypes.Secs24p8_initMsecs(TICKER_SYS_PERIOD_MS) / divided_by,
+        $cb(tickCbSys)
     )
-    expected_app_count = (divided_by * PRINT_PERIOD_MS) / APP_LED_PERIOD_MS
-    expected_sys_count = (divided_by * PRINT_PERIOD_MS) / SYS_LED_PERIOD_MS
+    expected_count_app = (divided_by * TICKER_PRINT_PERIOD_MS) / TICKER_APP_PERIOD_MS
+    expected_count_sys = (divided_by * TICKER_PRINT_PERIOD_MS) / TICKER_SYS_PERIOD_MS
 }
 
 function startPrintTicker() {
-    print_ticker.$$.$$.start(
-        TimeTypes.Secs24p8_initMsecs(PRINT_PERIOD_MS),
-        $cb(printTickCb)
+    ticker_print.$$.$$.start(
+        TimeTypes.Secs24p8_initMsecs(TICKER_PRINT_PERIOD_MS),
+        $cb(tickCbPrint)
     )
 }
 
 function stopLedTickers() {
-    app_ticker.$$.$$.stop()
-    sys_ticker.$$.$$.stop()
-    expected_app_count = 0
-    expected_sys_count = 0
+    ticker_app.$$.$$.stop()
+    ticker_sys.$$.$$.stop()
+    expected_count_app = 0
+    expected_count_sys = 0
 }
 
-function sysTickCb() {
-    sys_count += 1
-    $['%%d']
-    SysLed.$$.wink(10)
+function tickCbApp() {
+    count_app += 1
+    led_app.$$.wink(10)
+}
+
+function tickCbPrint() {
+    count_print += 1
+    prints_after_rate_change++
+    let this_count_app = count_app - last_count_app
+    let this_count_sys = count_sys - last_count_sys
+    let this_app_error = countError(this_count_app, expected_count_app)
+    let this_sys_error = countError(this_count_sys, expected_count_sys)
+    if (prints_after_rate_change > 2 && (this_app_error || this_sys_error)) {
+        this_app_error && total_errors++
+        this_sys_error && total_errors++
+    }
+    printTime(Common.Uptimer.$$.read())
+    printf` Print tick {rate: %dx, ticks: {app: %d%s, sys: %d%s}, errors: %d}\n`(
+        divided_by,
+        this_count_app,
+        this_app_error ? t$`*` : t$``,
+        this_count_sys,
+        this_sys_error ? t$`*` : t$``,
+        total_errors
+    )
+    if (divided_by > 0 && last_count_sys > 0 && last_count_sys == count_sys) {
+        printf`No sys ticks detected since last print\n`()
+        halt()
+    }
+    if (divided_by > 0 && last_count_app > 0 && last_count_app == count_app) {
+        printf`No app ticks detected since last print\n`()
+        halt()
+    }
+    last_count_app = count_app
+    last_count_sys = count_sys
+}
+
+function tickCbSys() {
+    count_sys += 1
+    led_sys.$$.wink(10)
 }
