774ef79d1c9473f98c7082717888136d5c78861f
build-sdk.yml workflow <hello@openem.org>
2025-06-17 08:51:32 +0000

    build-sdk run: 26.2.0.202506170851

--- workspace/adi.max326xx/adi.mcu.max326xx/ConsoleUart3.em.ts
+++ workspace/adi.max326xx/adi.mcu.max326xx/ConsoleUart3.em.ts
@@ -14,10 +14,11 @@ export const baud = $config<u32>(115200)
 const clkdiv = $config<u32>()
 
 export namespace em$meta {
-    const PCLK_FREQ = 50_000_000
+
+    const IBRO_FREQ = 7_372_800
 
     export function em$construct() {
-        clkdiv.$$val = Math.round(PCLK_FREQ / baud)
+        clkdiv.$$val = Math.round(IBRO_FREQ / baud)
         Idle.em$meta.addSleepEnter($cb(sleepEnter))
         Idle.em$meta.addSleepLeave($cb(sleepLeave))
     }
@@ -41,12 +42,15 @@ export function put(data: u8): void {
 function sleepEnter() {
     TxPin.reset()
     $R.LPGCR.PCLKDIS.$$ |= ($R.F_LPGCR_PCLKDIS_UART3 | $R.F_LPGCR_PCLKDIS_GPIO2)
+    $R.GCR.CLKCTRL.$$ &= ~$R.F_GCR_CLKCTRL_IBRO_EN
 }
 
 function sleepLeave() {
     $R.LPGCR.PCLKDIS.$$ &= ~($R.F_LPGCR_PCLKDIS_UART3 | $R.F_LPGCR_PCLKDIS_GPIO2)
     TxPin.makeOutput()
     TxPin.functionSelect(2)
+    $R.GCR.CLKCTRL.$$ |= $R.F_GCR_CLKCTRL_IBRO_EN
+    $R.UART3.OSR.$$ = 5
     $R.UART3.CLKDIV.$$ = clkdiv
     $R.UART3.CTRL.$$ |=
         $R.S_UART_CTRL_CHAR_SIZE_8BITS |
